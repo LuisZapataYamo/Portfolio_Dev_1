90397fa4542a676e4f65164a90aceb8565180426
jhonayquer <jhonayquer@gmail.com>
2023-04-21 16:21:43 -0500

    add link to buttoms in front-page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -14,13 +14,12 @@ import { useNavigate } from 'react-router-dom'
 const Home = () => {
   const navigate = useNavigate()
   const handleClick = (event, id) => {
-    event.preventDefault();
+    event.preventDefault()
     setTimeout(() => {
-      const element = document.getElementById(id);
-      element.scrollIntoView({ "behavior": "smooth" });
+      const element = document.getElementById(id)
+      element.scrollIntoView({ 'behavior': 'smooth' })
     }, 300)
   }
-
 
   return (
     <div className="main">
@@ -49,32 +48,34 @@ const Home = () => {
               </div>
               <ul className="list-buttoms">
                 <li>
-                  <LinkedinSvg className="icon"/>
-                  <span className="button-social">Linkedin</span>
+                  <a href="https://www.linkedin.com/in/luiszapatayamo/" rel='noopener' target='_blank'>
+                    <LinkedinSvg className="icon"/>
+                    <span className="button-social">Linkedin</span>
+                  </a>
                 </li>
                 <li>
-                  <GithubSvg className="icon"/>
-                  <span className="button-social">GitHub</span>
+                  <a href="https://github.com/LuisZapataYamo" rel='noopener' target='_blank'>
+                    <GithubSvg className="icon"/>
+                    <span className="button-social">GitHub</span>
+                  </a>
                 </li>
               </ul>
             </div>
-            <div className="cube3d">
-            </div>
           </div>
         </div>
-        <div className="about" id='about'>
-          <div className='content-about'>
-          <div className="content-text">
-            <h2>Sobre mi</h2>
-            <p>Joven estudiante de ingeniería de sistemas capaz, carismático, respetuoso y productivo con ganas de
-              aportar a la comunidad para así poder desarrollarse personalmente y profesionalmente. Con convicciones
-              hacia un mejor futuro y ganas de adquirir más conocimientos sobre las TIC.</p>
-            <p>Actualmente estoy trabajando en la creacion de un blog para que estudiante de mi universidad puedan
-              compartir conocimientos.</p>
+        <div className="about" id="about">
+          <div className="content-about">
+            <div className="content-text">
+              <h2>Sobre mi</h2>
+              <p>Joven estudiante de ingeniería de sistemas capaz, carismático, respetuoso y productivo con ganas de
+                aportar a la comunidad para así poder desarrollarse personalmente y profesionalmente. Con convicciones
+                hacia un mejor futuro y ganas de adquirir más conocimientos sobre las TIC.</p>
+              <p>Actualmente estoy trabajando en la creacion de un blog para que estudiante de mi universidad puedan
+                compartir conocimientos.</p>
+            </div>
+            <img src={perfilPNG} alt=""/>
           </div>
-          <img src={perfilPNG} alt=""/>
-          </div>
-          <div className="skills" id='skills'>
+          <div className="skills" id="skills">
             <SphereContent lista={[
               'Python',
               'Django',
@@ -98,22 +99,25 @@ const Home = () => {
                 eficiencia de los procesos mediante pruebas automatizadas en la web.
               </p>
               <p>También he trabajado con Python, Pandas, HTML y CSS para el análisis y manipulación de datos, lo que me
-                permite extraer información valiosa a partir de grandes conjuntos de datos y crear diseños web atractivos.
+                permite extraer información valiosa a partir de grandes conjuntos de datos y crear diseños web
+                atractivos.
                 Por último, tengo experiencia en el manejo de Git para el trabajo colaborativo en proyectos de
                 software.</p>
             </div>
           </div>
         </div>
-        <div className="worked" id='worked'>
+        <div className="worked" id="worked">
           <h2>Experiencia</h2>
           <div className="work">
             <div className="text-work">
-              <h3>Desarrollador de Back-End / Soporte Tecnico <span>@<a href='https://valhalla.com.pe/' target='_blank' rel="noopener">Valhalla</a></span></h3>
+              <h3>Desarrollador de Back-End / Soporte Tecnico <span>@<a href="https://valhalla.com.pe/" target="_blank"
+                                                                        rel="noopener">Valhalla</a></span></h3>
               <h4>Sept. 2022 - actualidad</h4>
-              <p>Desarrollo y mantenimiento del back-end del proyecto Valhalla. Soporte técnico de páginas webs usando WordPress y Shopify.</p>
+              <p>Desarrollo y mantenimiento del back-end del proyecto Valhalla. Soporte técnico de páginas webs usando
+                WordPress y Shopify.</p>
               <div className="features">
                 <span>Aptitudes:</span>
-                <ul className='tags'>
+                <ul className="tags">
                   <li>#Python</li>
                   <li>#Django</li>
                   <li>#Django REST framework</li>
@@ -129,9 +133,9 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div className="education" id='education'>
+        <div className="education" id="education">
           <h2>Educacion</h2>
-          <div className="study" >
+          <div className="study">
             <div className="text-study">
               <h3>Universidad Nacional Tecnologica de Lima Sur</h3>
               <p>Estudios en Ingenieria de Sistemas | 2019 - Provisto a finalizar en 2023</p>
@@ -143,7 +147,7 @@ const Home = () => {
         </div>
       </div>
     </div>
-)
+  )
 }
 
 export default Home
