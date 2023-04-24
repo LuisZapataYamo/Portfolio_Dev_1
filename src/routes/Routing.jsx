0ce969f93db3f9ec6198eeacfcba0104c96deda1
jhonayquer <jhonayquer@gmail.com>
2023-04-24 01:41:59 -0500

    add path for blog

--- src/routes/Routing.jsx
+++ src/routes/Routing.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from '../components/Home/Home.jsx'
+import BlogLayout from '../components/Blog/BlogLayout/BlogLayout.jsx'
+import BlogHome from '../components/Blog/BlogHome/BlogHome.jsx'
 
 const Routing = () => {
   return (
     <BrowserRouter basename="/LuisZapata">
       <Routes>
         <Route index path='/' element={<Home/>}/>
+        <Route element={<BlogLayout/>}>
+          <Route path='/blog/' element={<BlogHome/>}/>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
