87914fd1288891f7d53def5c50f8e4a0167818c4
jhonayquer <jhonayquer@gmail.com>
2023-04-29 16:39:23 -0500

    fix bugs for search in this url with diferent parameter q

--- src/components/Blog/SearchList/SearchList.jsx
+++ src/components/Blog/SearchList/SearchList.jsx
@@ -1,30 +1,32 @@
-import "./SearchList.css"
+import './SearchList.css'
 
 import { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import ArticleCard from '../ArticleCard/ArticleCard.jsx'
 
 const SearchList = () => {
-  const search = new URLSearchParams(useLocation().search).get('q')
+  const search = useLocation().search
   const [posts, setPosts] = useState()
 
   useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const q = urlParams.get('q');
     const fetchData = async () => {
       const response = await fetch('https://dev.to/api/articles?username=luiszapatayamo')
       let data = await response.json()
       if (search !== null && search !== '') {
-        data = data.filter((post) => post.title.toLowerCase().includes(search.toLowerCase()))
+        data = data.filter((post) => post.title.toLowerCase().includes(q.toLowerCase()))
       }
+      console.log()
       setPosts(data)
     }
-
     fetchData()
-  }, [])
+  }, [search])
 
   return (
     <div className="main-list">
       {posts?.map(post => (
-        <ArticleCard post={post}/>
+        <ArticleCard key={post.id} post={post}/>
       ))}
     </div>
   )
