c0a50002d95932f092ea5a42305ef1618c6035ff
Luis Zapata Yamo <jhonayquer@gmail.com>
2025-01-09 10:39:05 -0500

    Modificando descripcion

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -22,50 +22,50 @@ const Home = () => {
   }
 
   return (
-    <div className='main'>
-      <div className='header-home'>
-        <div className='content'>
-          <img src={logoHomeSvg} alt='logo-home' />
-          <ul className='navbar-home'>
-            <li><a href='#about' onClick={(event) => handleClick(event, 'about')}>Sobre mi</a></li>
-            <li><a href='#worked' onClick={(event) => handleClick(event, 'worked')}>Experiencia</a></li>
-            <li><a href='#education' onClick={(event) => handleClick(event, 'education')}>Educación</a></li>
-            <li><Link to='/blog/' className='button-blog'>Blog</Link></li>
+    <div className="main">
+      <div className="header-home">
+        <div className="content">
+          <img src={logoHomeSvg} alt="logo-home"/>
+          <ul className="navbar-home">
+            <li><a href="#about" onClick={(event) => handleClick(event, 'about')}>Sobre mi</a></li>
+            <li><a href="#worked" onClick={(event) => handleClick(event, 'worked')}>Experiencia</a></li>
+            <li><a href="#education" onClick={(event) => handleClick(event, 'education')}>Educación</a></li>
+            <li><Link to="/blog/" className="button-blog">Blog</Link></li>
           </ul>
         </div>
       </div>
-      <div className='main-home'>
-        <div className='content-front-page'>
-          <div className='front-page'>
+      <div className="main-home">
+        <div className="content-front-page">
+          <div className="front-page">
             <div>
-              <div className='text-content'>
-                <p className='hello'>Hola, me llamo</p>
-                <h1 className='name-title'>Luis Zapata</h1>
-                <p className='introduction'>Soy estudiante de ingenieria de sistemas y desarrollador back-end
+              <div className="text-content">
+                <p className="hello">Hola, me llamo</p>
+                <h1 className="name-title">Luis Zapata</h1>
+                <p className="introduction">Soy estudiante de ingenieria de sistemas y desarrollador back-end
                   continuamente estoy creando productos digitales que contentan
                   una buena experiencia y sean de utilidad para las personas
                 </p>
               </div>
-              <ul className='list-buttoms'>
+              <ul className="list-buttoms">
                 <li>
-                  <a href='https://www.linkedin.com/in/luiszapatayamodev/' rel='noopener noreferrer' target='_blank'>
-                    <LinkedinSvg className='icon' />
-                    <span className='button-social'>Linkedin</span>
+                  <a href="https://www.linkedin.com/in/luiszapatayamodev/" rel="noopener noreferrer" target="_blank">
+                    <LinkedinSvg className="icon"/>
+                    <span className="button-social">Linkedin</span>
                   </a>
                 </li>
                 <li>
-                  <a href='https://github.com/LuisZapataYamo' rel='noopener noreferrer' target='_blank'>
-                    <GithubSvg className='icon' />
-                    <span className='button-social'>GitHub</span>
+                  <a href="https://github.com/LuisZapataYamo" rel="noopener noreferrer" target="_blank">
+                    <GithubSvg className="icon"/>
+                    <span className="button-social">GitHub</span>
                   </a>
                 </li>
               </ul>
             </div>
           </div>
         </div>
-        <div className='about' id='about'>
-          <div className='content-about'>
-            <div className='content-text'>
+        <div className="about" id="about">
+          <div className="content-about">
+            <div className="content-text">
               <h2>Sobre mi</h2>
               <p>Joven estudiante de ingeniería de sistemas capaz, carismático, respetuoso y productivo con ganas de
                 aportar a la comunidad para así poder desarrollarse personalmente y profesionalmente. Con convicciones
@@ -76,15 +76,15 @@ const Home = () => {
               </p>
             </div>
             <picture>
-              <img src={perfilPNG} alt='' />
+              <img src={perfilPNG} alt=""/>
             </picture>
           </div>
-          <div className='skills' id='skills'>
+          <div className="skills" id="skills">
             <SphereContent
               lista={['Python', 'Django', 'Django REST', 'HTML', 'CSS', 'Git', 'PostgreSQL', 'Three.js', 'Java', 'C++', 'Selenium', 'Pandas', 'Figma', 'JavaScript']}
-              color='#F3F2EF'
+              color="#F3F2EF"
             />
-            <div className='content-text'>
+            <div className="content-text">
               <h2>Habilidades</h2>
               <p>Tengo experiencia en la creación de aplicaciones web escalables y robustas utilizando herramientas
                 tecnológicas como Django Rest framework. Además, he utilizado Selenium para optimizar y mejorar la
@@ -99,82 +99,94 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div className='worked' id='worked'>
+        <div className="worked" id="worked">
           <h2>Experiencia</h2>
-          <div className='work pya'>
-            <div className='text-work'>
+          <div className="work pya">
+            <div className="text-work">
               <h3>Desarrollador de Software
                 <span>@
                   <a
-                    href='https://www.pya.cl/' target='_blank'
-                    rel='noopener noreferrer'
+                    href="https://www.pya.cl/" target="_blank"
+                    rel="noopener noreferrer"
                   >
                     Paréz&Alvarez
                   </a>
                 </span>
               </h3>
-              <h4>Ener. 2024 - Actualidad</h4>
-              <p>Desarrollo y mantenimiento del sistema legacy, tambien migracion de modulos a sistema basado en microservicios.
+              <h4>Ener. 2024 - Dic 2024</h4>
+              <p>Encargado de resolver problemas de usuarios y brindar soporte técnico para sistemas internos,
+                asegurando su correcto funcionamiento. Experiencia en el mantenimiento y optimización de sistemas legacy
+                mediante la implementación de buenas prácticas de programación, así como en la migración de módulos
+                antiguos hacia una arquitectura moderna basada en microservicios. Participación activa en la corrección
+                y mejora de funcionalidades utilizando tecnologías como Spring Boot, WebFlux y Angular
               </p>
-              <div className='features'>
+              <div className="features">
                 <span>Aptitudes:</span>
-                <ul className='tags'>
+                <ul className="tags">
                   <li>#Java</li>
                   <li>#Spring Boot</li>
+                  <li>#Spring Web Flux</li>
+                  <li>#Microsoft Azure</li>
                   <li>#Airflow</li>
                   <li>#Jenkins</li>
                   <li>#PostgreSQL</li>
                   <li>#Angular</li>
                   <li>#Linux</li>
+                  <li>#Python</li>
                 </ul>
               </div>
             </div>
-            <div className='logo'>
-              <img src={pyaPNG} alt='' />
+            <div className="logo">
+              <img src={pyaPNG} alt=""/>
             </div>
           </div>
-          <div className='work'>
-            <div className='text-work'>
+          <div className="work">
+            <div className="text-work">
               <h3>Desarrollador de Back-End / Soporte Tecnico
                 <span>@
                   <a
-                    href='https://valhalla.com.pe/' target='_blank'
-                    rel='noopener noreferrer'
+                    href="https://valhalla.com.pe/" target="_blank"
+                    rel="noopener noreferrer"
                   >Valhalla
                   </a>
                 </span>
               </h3>
               <h4>Sept. 2022 - Ener. 2024</h4>
-              <p>Desarrollo y mantenimiento del back-end del proyecto Valhalla. Soporte técnico de páginas webs usando
-                WordPress y Shopify.
+              <p>Responsable de resolver errores y desarrollar nuevas APIs para Zeia Smart Wellness, asegurando el uso
+                de buenas prácticas. Participé en la migración del sistema a AWS, incluyendo la configuración de
+                dominios y subdominios. Optimizé bases de datos utilizando Amazon Aurora Postgres e integré nuevos
+                modelos de datos. Además, desarrollé tareas asíncronas para el monitoreo de dispositivos y la gestión de
+                notificaciones, colaboré en la creación de componentes con React y documenté las APIs implementadas.
               </p>
-              <div className='features'>
+              <div className="features">
                 <span>Aptitudes:</span>
-                <ul className='tags'>
+                <ul className="tags">
+                  <li>#AWS</li>
+                  <li>#PostgreSQL</li>
+                  <li>#Java</li>
+                  <li>#Sprint Boot</li>
                   <li>#Python</li>
-                  <li>#Django</li>
                   <li>#Django REST framework</li>
                   <li>#Celery</li>
-                  <li>#PostgreSQL</li>
-                  <li>#WordPress</li>
+                  <li>#React.js</li>
                   <li>#Shopify</li>
                 </ul>
               </div>
             </div>
-            <div className='logo'>
-              <img src={valhallaPNG} alt='' />
+            <div className="logo">
+              <img src={valhallaPNG} alt=""/>
             </div>
           </div>
         </div>
-        <div className='education' id='education'>
+        <div className="education" id="education">
           <h2>Educacion</h2>
-          <div className='study'>
-            <div className='text-study'>
+          <div className="study">
+            <div className="text-study">
               <h3>Universidad Nacional Tecnologica de Lima Sur</h3>
               <p>Estudios en Ingenieria de Sistemas | 2019 - Provisto a finalizar en 2023</p>
             </div>
-            <div className='logo'>
-              <img src={untelsPNG} alt='' />
+            <div className="logo">
+              <img src={untelsPNG} alt=""/>
             </div>
           </div>
         </div>
