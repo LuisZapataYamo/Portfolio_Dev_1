f89172bb718e7f95df58683ec74719295449538b
Luis Zapata Yamo <jhonayquer@gmail.com>
2025-01-09 10:46:41 -0500

    Modificando descripcion

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -41,7 +41,7 @@ const Home = () => {
               <div className="text-content">
                 <p className="hello">Hola, me llamo</p>
                 <h1 className="name-title">Luis Zapata</h1>
-                <p className="introduction">Soy estudiante de ingenieria de sistemas y desarrollador back-end
+                <p className="introduction">Soy bachiller en ingenieria de sistemas y desarrollador back-end
                   continuamente estoy creando productos digitales que contentan
                   una buena experiencia y sean de utilidad para las personas
                 </p>
@@ -67,12 +67,10 @@ const Home = () => {
           <div className="content-about">
             <div className="content-text">
               <h2>Sobre mi</h2>
-              <p>Joven estudiante de ingeniería de sistemas capaz, carismático, respetuoso y productivo con ganas de
-                aportar a la comunidad para así poder desarrollarse personalmente y profesionalmente. Con convicciones
-                hacia un mejor futuro y ganas de adquirir más conocimientos sobre las TIC.
-              </p>
-              <p>Actualmente estoy trabajando en la creacion de un blog para que estudiante de mi universidad puedan
-                compartir conocimientos.
+              <p>Bachiller de ingeniería de sistemas con interés en el aprendizaje continuo y el desarrollo personal y
+                profesional. Apasionado por la naturaleza y motivado por contribuir positivamente a la comunidad a
+                través de la tecnología. Enfocado en adquirir nuevos conocimientos sobre las TIC y en aplicarlos para
+                construir soluciones sostenibles y orientadas a un mejor futuro.
               </p>
             </div>
             <picture>
@@ -86,15 +84,11 @@ const Home = () => {
             />
             <div className="content-text">
               <h2>Habilidades</h2>
-              <p>Tengo experiencia en la creación de aplicaciones web escalables y robustas utilizando herramientas
-                tecnológicas como Django Rest framework. Además, he utilizado Selenium para optimizar y mejorar la
-                eficiencia de los procesos mediante pruebas automatizadas en la web.
-              </p>
-              <p>También he trabajado con Python, Pandas, HTML y CSS para el análisis y manipulación de datos, lo que me
-                permite extraer información valiosa a partir de grandes conjuntos de datos y crear diseños web
-                atractivos.
-                Por último, tengo experiencia en el manejo de Git para el trabajo colaborativo en proyectos de
-                software.
+              <p>Cuento con experiencia en el desarrollo de aplicaciones web escalables y en la optimización de procesos
+                mediante pruebas automatizadas. También he trabajado en el análisis y manipulación de datos, lo que me
+                permite extraer información útil de grandes volúmenes de datos y desarrollar interfaces web funcionales
+                y atractivas. Además, tengo experiencia en el trabajo colaborativo en proyectos de software, siguiendo
+                buenas prácticas de control de versiones.
               </p>
             </div>
           </div>
@@ -183,7 +177,7 @@ const Home = () => {
           <div className="study">
             <div className="text-study">
               <h3>Universidad Nacional Tecnologica de Lima Sur</h3>
-              <p>Estudios en Ingenieria de Sistemas | 2019 - Provisto a finalizar en 2023</p>
+              <p>Estudios en Ingenieria de Sistemas | 2019 - 2024</p>
             </div>
             <div className="logo">
               <img src={untelsPNG} alt=""/>
